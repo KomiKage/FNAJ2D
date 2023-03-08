3bf0a134154409a8985de4254805e22e94ef7ff3
Jochem van der Steen <75484259+jochemvdsteen@users.noreply.github.com>
2023-03-08 17:23:21 +0100

    .

---
+++ Assets/Scripts/Cameras.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Camera office;
+    public Camera cam1;
+    public Camera cam2;
+    public Camera cam3;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
